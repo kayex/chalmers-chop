b66b90075d46601f638ca0c6163ec9f7e898d73b
Johan Vester <jv@jv-macbook.local>
2017-01-12 14:39:49 +0100

    Add TodaysMenu()

--- menu.go
+++ menu.go
@@ -1,5 +1,7 @@
 package chalmers_chop
 
+import "time"
+
 type Menu struct {
 	Title  string `json:"title"`
 	Date   string `json:"date"`
@@ -15,4 +17,26 @@ func NewMenu(title string, date string) *Menu {
 
 func (m *Menu) AddDish(dish Dish) {
 	m.Dishes = append(m.Dishes, dish)
+}
+
+func (r *Restaurant) TodaysMenu() *Menu {
+	found, menu := r.findMenuByTime(time.Now())
+
+	if !found {
+		return nil
+	}
+
+	return menu
+}
+
+func (r *Restaurant) findMenuByTime(time time.Time) (bool, *Menu) {
+	date := time.Format("2006-01-02")
+
+	for _, m := range r.Menus {
+		if m.Date == date {
+			return true, &m
+		}
+	}
+
+	return false, nil
 }
