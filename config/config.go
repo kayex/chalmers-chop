e9265647188ceb173df1fae6b1c078dad63640ec
Johan Vester <jv@jv-macbook.local>
2017-01-12 17:24:34 +0100

    Implement JSON HTTP export

--- config/config.go
+++ config/config.go
@@ -4,6 +4,12 @@ type RestaurantConfig struct {
 	MenuURLs []string `toml:"rss"`
 }
 
+type ExportConfig struct {
+	URL string `toml:"url"`
+	Token string
+}
+
 type Config struct {
+	ExportConfig ExportConfig `toml:"export"`
 	RestaurantConfig RestaurantConfig `toml:"restaurants"`
 }
---
+++ export.go
@@ -0,0 +1,50 @@
+package chalmers_chop
+
+import (
+	"net/http"
+	"bytes"
+	"fmt"
+	"errors"
+)
+
+type Exporter interface {
+	Export(json []byte) error
+}
+
+type POSTExporter struct {
+	url string
+	token string
+}
+
+func NewPOSTExporter(url, token string) *POSTExporter {
+	return &POSTExporter{
+		url: url,
+		token: token,
+	}
+}
+
+func (e *POSTExporter) Export(json []byte) error {
+	resp := postJson(json, e.url, e.token)
+
+	if !(resp.StatusCode == http.StatusOK || resp.StatusCode == http.StatusCreated || resp.StatusCode == http.StatusNoContent) {
+		msg := fmt.Sprintf("POST Export failed: %v (%v)", resp.Status, resp.Body)
+		return errors.New(msg)
+	}
+
+	return nil
+}
+
+func postJson(json []byte, url, token string) *http.Response {
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(json))
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", "Token " + token)
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	return resp
+}
