143cdb78cffd6fee0d836200cf8261c296154169
Johan Vester <jv@jv-macbook.local>
2017-01-12 18:35:27 +0100

    Go fmt

--- config/config.go
+++ config/config.go
@@ -5,11 +5,11 @@ type RestaurantConfig struct {
 }
 
 type ExportConfig struct {
-	URL string `toml:"url"`
+	URL   string `toml:"url"`
 	Token string
 }
 
 type Config struct {
-	ExportConfig ExportConfig `toml:"export"`
-	RestaurantConfig RestaurantConfig `toml:"restaurants"`
+	ExportConfig ExportConfig          `toml:"export"`
+	AreaConfigs  map[string]AreaConfig `toml:"restaurants"`
 }
